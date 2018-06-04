e1b64afaf198e7bf972e7d72c5712acd6c20c35c
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-06-04 20:46:50 +0300

    Add mpp reader/writer functions, add JVM utility functions

---
+++ kotlinx-coroutines-io/src/main/kotlin/kotlinx/coroutines/experimental/io/Coroutines.kt
@@ -0,0 +1,52 @@
+package kotlinx.coroutines.experimental.io
+
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.*
+
+/**
+ * A coroutine job that is reading from a byte channel
+ */
+interface ReaderJob : Job {
+    /**
+     * A reference to the channel that this coroutine is reading from
+     */
+    val channel: ByteWriteChannel
+}
+
+/**
+ * A coroutine job that is writing to a byte channel
+ */
+interface WriterJob : Job {
+    /**
+     * A reference to the channel that this coroutine is writing to
+     */
+    val channel: ByteReadChannel
+}
+
+interface ReaderScope : CoroutineScope {
+    val channel: ByteReadChannel
+}
+
+interface WriterScope : CoroutineScope {
+    val channel: ByteWriteChannel
+}
+
+expect fun reader(coroutineContext: CoroutineContext,
+                      channel: ByteChannel,
+                      parent: Job? = null,
+                      block: suspend ReaderScope.() -> Unit): ReaderJob
+
+expect fun reader(coroutineContext: CoroutineContext,
+                  autoFlush: Boolean = false,
+                  parent: Job? = null,
+                  block: suspend ReaderScope.() -> Unit): ReaderJob
+
+expect fun writer(coroutineContext: CoroutineContext,
+                      channel: ByteChannel,
+                      parent: Job? = null,
+                      block: suspend WriterScope.() -> Unit): WriterJob
+
+expect fun writer(coroutineContext: CoroutineContext,
+                  autoFlush: Boolean = false,
+                  parent: Job? = null,
+                  block: suspend WriterScope.() -> Unit): WriterJob
