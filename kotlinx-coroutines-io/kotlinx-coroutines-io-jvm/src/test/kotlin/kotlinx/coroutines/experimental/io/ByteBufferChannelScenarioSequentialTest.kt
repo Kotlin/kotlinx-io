0248b735f50118770bba40438e1f674bfc15a214
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-06-09 13:28:44 +0300

    Add tests

---
+++ kotlinx-coroutines-io/kotlinx-coroutines-io-jvm/src/test/kotlin/kotlinx/coroutines/experimental/io/ByteBufferChannelScenarioSequentialTest.kt
@@ -0,0 +1,10 @@
+package kotlinx.coroutines.experimental.io
+
+import kotlinx.io.core.*
+
+class ByteBufferChannelScenarioSequentialTest : ByteBufferChannelScenarioTest() {
+
+    override fun ByteChannel(autoFlush: Boolean): ByteChannel {
+        return ByteChannelSequentialJVM(BufferView.Empty, autoFlush)
+    }
+}
---
+++ kotlinx-coroutines-io/kotlinx-coroutines-io-jvm/src/test/kotlin/kotlinx/coroutines/experimental/io/StringsSequentialTest.kt
@@ -0,0 +1,9 @@
+package kotlinx.coroutines.experimental.io
+
+import kotlinx.io.core.*
+
+class StringsSequentialTest : StringsTest() {
+    override fun ByteChannel(autoFlush: Boolean): ByteChannel {
+        return ByteChannelSequentialJVM(BufferView.Empty, autoFlush)
+    }
+}
