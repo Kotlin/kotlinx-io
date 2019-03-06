5041ff060b08d2234f77ba8de6bed354b45d8830
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-03-06 20:42:50 +0300

    Support ISO-8859-1 in common
    (cherry-picked from 3ba7ce7030179b9ed80b539ee62116ad18039d35)

--- common/src/kotlinx/io/charsets/Encoding.kt
+++ common/src/kotlinx/io/charsets/Encoding.kt
@@ -111,6 +111,7 @@ expect fun CharsetDecoder.decodeExactBytes(input: Input, inputLength: Int): Stri
 // ----------------------------- REGISTRY ------------------------------------------------------------------------------
 expect object Charsets {
     val UTF_8: Charset
+    val ISO_8859_1: Charset
 }
 
 expect class MalformedInputException(message: String) : Throwable
