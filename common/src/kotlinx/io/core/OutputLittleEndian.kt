9202b05d574dc465bfa006c9aa6ac30fd3f8fba5
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-03-11 15:50:34 +0300

    Fix byte order deprecation helpers

--- common/src/kotlinx/io/core/OutputLittleEndian.kt
+++ common/src/kotlinx/io/core/OutputLittleEndian.kt
@@ -1,89 +1,82 @@
-@file:Suppress("Duplicates")
-
 package kotlinx.io.core
 
 import kotlinx.io.bits.*
 
 fun Output.writeShortLittleEndian(value: Short) {
-    writePrimitiveTemplate(value, { writeShort(value) }, { reverseByteOrder() })
+    writePrimitiveTemplate(value, { writeShort(it) }, { reverseByteOrder() })
 }
 
 fun Output.writeIntLittleEndian(value: Int) {
-    writePrimitiveTemplate(value, { writeInt(value) }, { reverseByteOrder() })
+    writePrimitiveTemplate(value, { writeInt(it) }, { reverseByteOrder() })
 }
 
 fun Output.writeLongLittleEndian(value: Long) {
-    writePrimitiveTemplate(value, { writeLong(value) }, { reverseByteOrder() })
+    writePrimitiveTemplate(value, { writeLong(it) }, { reverseByteOrder() })
 }
 
 fun Output.writeFloatLittleEndian(value: Float) {
-    writePrimitiveTemplate(value, { writeFloat(value) }, { reverseByteOrder() })
+    writePrimitiveTemplate(value, { writeFloat(it) }, { reverseByteOrder() })
 }
 
 fun Output.writeDoubleLittleEndian(value: Double) {
-    writePrimitiveTemplate(value, { writeDouble(value) }, { reverseByteOrder() })
+    writePrimitiveTemplate(value, { writeDouble(it) }, { reverseByteOrder() })
 }
 
-fun Output.writeFullyLittleEndian(dst: UShortArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst.asShortArray(), offset, length)
+fun Output.writeFullyLittleEndian(source: UShortArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeFullyLittleEndian(source.asShortArray(), offset, length)
 }
 
-fun Output.writeFullyLittleEndian(dst: ShortArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst, offset, length)
-    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
-        val lastIndex = offset + length - 1
-        for (index in offset..lastIndex) {
-            dst[index] = dst[index].reverseByteOrder()
-        }
-    }
+fun Output.writeFullyLittleEndian(source: ShortArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeArrayTemplate(
+        offset,
+        length,
+        2,
+        { writeFully(source, offset, length) },
+        { writeShort(source[it].reverseByteOrder()) })
 }
 
-fun Output.writeFullyLittleEndian(dst: UIntArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst.asIntArray(), offset, length)
+fun Output.writeFullyLittleEndian(source: UIntArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeFullyLittleEndian(source.asIntArray(), offset, length)
 }
 
-fun Output.writeFullyLittleEndian(dst: IntArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst, offset, length)
-    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
-        val lastIndex = offset + length - 1
-        for (index in offset..lastIndex) {
-            dst[index] = dst[index].reverseByteOrder()
-        }
-    }
+fun Output.writeFullyLittleEndian(source: IntArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeArrayTemplate(
+        offset,
+        length,
+        4,
+        { writeFully(source, offset, length) },
+        { writeInt(source[it].reverseByteOrder()) })
 }
 
-fun Output.writeFullyLittleEndian(dst: ULongArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst.asLongArray(), offset, length)
+fun Output.writeFullyLittleEndian(source: ULongArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeFullyLittleEndian(source.asLongArray(), offset, length)
 }
 
-fun Output.writeFullyLittleEndian(dst: LongArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst, offset, length)
-    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
-        val lastIndex = offset + length - 1
-        for (index in offset..lastIndex) {
-            dst[index] = dst[index].reverseByteOrder()
-        }
-    }
+fun Output.writeFullyLittleEndian(source: LongArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeArrayTemplate(
+        offset,
+        length,
+        8,
+        { writeFully(source, offset, length) },
+        { writeLong(source[it].reverseByteOrder()) })
 }
 
-fun Output.writeFullyLittleEndian(dst: FloatArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst, offset, length)
-    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
-        val lastIndex = offset + length - 1
-        for (index in offset..lastIndex) {
-            dst[index] = dst[index].reverseByteOrder()
-        }
-    }
+fun Output.writeFullyLittleEndian(source: FloatArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeArrayTemplate(
+        offset,
+        length,
+        4,
+        { writeFully(source, offset, length) },
+        { writeFloat(source[it].reverseByteOrder()) })
 }
 
-fun Output.writeFullyLittleEndian(dst: DoubleArray, offset: Int = 0, length: Int = dst.size - offset) {
-    writeFully(dst, offset, length)
-    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
-        val lastIndex = offset + length - 1
-        for (index in offset..lastIndex) {
-            dst[index] = dst[index].reverseByteOrder()
-        }
-    }
+fun Output.writeFullyLittleEndian(source: DoubleArray, offset: Int = 0, length: Int = source.size - offset) {
+    writeArrayTemplate(
+        offset,
+        length,
+        8,
+        { writeFully(source, offset, length) },
+        { writeDouble(source[it].reverseByteOrder()) })
 }
 
 private fun <T : Any> Output.writePrimitiveTemplate(value: T, write: (T) -> Unit, reverse: T.() -> T) {
@@ -95,6 +88,34 @@ private fun <T : Any> Output.writePrimitiveTemplate(value: T, write: (T) -> Unit
     )
 }
 
-@Suppress("DEPRECATION")
+
+private inline fun Output.writeArrayTemplate(
+    offset: Int,
+    length: Int,
+    componentSize: Int,
+    writeFullyBE: () -> Unit,
+    writeComponent: Buffer.(Int) -> Unit
+) {
+    if (byteOrderDeprecated != ByteOrder.LITTLE_ENDIAN) {
+        val untilIndex = offset + length
+        var start = offset
+        writeWhileSize(componentSize) { buffer ->
+            val size = minOf(buffer.writeRemaining / componentSize, untilIndex - start)
+            val lastIndex = start + size - 1
+            for (index in start..lastIndex) {
+                writeComponent(buffer, index)
+            }
+            start += size
+            when {
+                start < untilIndex -> componentSize
+                else -> 0
+            }
+        }
+    } else {
+        writeFullyBE()
+    }
+}
+
+@Suppress("DEPRECATION_ERROR")
 private inline val Output.byteOrderDeprecated
     get() = byteOrder
