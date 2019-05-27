5d17eca8915d7db017467c5306823da9a7704ba4
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-05-27 12:02:33 +0300

    Detach pool from allocator

--- core/commonMain/src/kotlinx/io/buffer/BufferAllocator.kt
+++ core/commonMain/src/kotlinx/io/buffer/BufferAllocator.kt
@@ -1,7 +1,5 @@
 package kotlinx.io.buffer
 
-import kotlinx.io.pool.*
-
 interface BufferAllocator {
     fun allocate(size: Int): Buffer
 
@@ -9,15 +7,3 @@ interface BufferAllocator {
 }
 
 expect object PlatformBufferAllocator : BufferAllocator
-
-internal const val DEFAULT_BUFFER_SIZE: Int = 1024
-internal const val DEFAULT_POOL_CAPACITY: Int = 16
-
-internal class BufferPool(
-    private val bufferSize: Int = DEFAULT_BUFFER_SIZE,
-    private val source: BufferAllocator = PlatformBufferAllocator
-) : DefaultPool<Buffer>(DEFAULT_POOL_CAPACITY) {
-    override fun produceInstance(): Buffer {
-        return source.allocate(bufferSize)
-    }
-}
