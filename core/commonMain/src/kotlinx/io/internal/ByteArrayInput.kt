59f5898710a14683f77911074424cbcd2ea7a84b
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-11-14 12:38:34 +0300

    Introduce ByteArrayInput

---
+++ core/commonMain/src/kotlinx/io/internal/ByteArrayInput.kt
@@ -0,0 +1,25 @@
+package kotlinx.io.internal
+
+import kotlinx.io.*
+import kotlinx.io.buffer.*
+
+internal class ByteArrayInput(
+    private val source: ByteArray, startIndex: Int,
+    private val endIndex: Int
+) : Input() {
+
+    private var currentIndex = startIndex
+
+    override fun closeSource() {
+        // Nothing by default
+    }
+
+    override fun fill(buffer: Buffer): Int {
+        val size = (endIndex - currentIndex).coerceAtMost(buffer.size)
+        repeat(size) {
+            buffer[it] = source[currentIndex]
+            ++currentIndex
+        }
+        return size
+    }
+}
