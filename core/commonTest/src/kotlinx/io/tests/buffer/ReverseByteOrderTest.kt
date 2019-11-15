34818ee734163e82a33a6eb3eebe19a6ef85effa
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-11-15 15:07:36 +0300

    Fix typo

--- core/commonTest/src/kotlinx/io/tests/buffer/ReverseByteOrderTest.kt
+++ core/commonTest/src/kotlinx/io/tests/buffer/ReverseByteOrderTest.kt
@@ -21,7 +21,7 @@ class ReverseByteOrderTest {
         val buffer = PlatformBufferAllocator.allocate(8)
         try {
             buffer.storeLongAt(0, value, ByteOrder.BIG_ENDIAN)
-            assertEquals(value, buffer.loadLongAt(0, ByteOrder.BIG_ENDIAN))\
+            assertEquals(value, buffer.loadLongAt(0, ByteOrder.BIG_ENDIAN))
             assertEquals(reversed, buffer.loadLongAt(0, ByteOrder.LITTLE_ENDIAN))
             buffer.storeLongAt(0, value, ByteOrder.LITTLE_ENDIAN)
             assertEquals(reversed, buffer.loadLongAt(0, ByteOrder.BIG_ENDIAN))
