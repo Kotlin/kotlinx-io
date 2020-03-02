6edf2c07346e8c73d0f27784bfaaaf9732e4c47e
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-03-02 16:17:44 +0300

    Prevent redundant buffer borrow

--- core/commonMain/src/kotlinx/io/Input.kt
+++ core/commonMain/src/kotlinx/io/Input.kt
@@ -472,7 +472,11 @@ public abstract class Input : Closeable {
         }
 
         val bytes = preview ?: startPreview()
-        return readThroughPreview(bytes)
+        return fillAndStoreInPreview(bytes)
+    }
+
+    private fun prepareNewBuffer() {
+        buffer = bufferPool.borrow()
     }
 
     /**
@@ -492,13 +496,14 @@ public abstract class Input : Closeable {
      * The [bytes] shouldn't be empty.
      */
     private fun fetchFromPreviewAndDiscard(bytes: Bytes): Int {
-        bufferPool.recycle(buffer)
         bytes.discardFirst()
 
         if (bytes.isEmpty()) {
             previewBytes = null
             return fillFromSource()
         }
+
+        bufferPool.recycle(buffer)
 
         bytes.pointed(0) { newBuffer, newLimit ->
             position = 0
@@ -509,12 +514,13 @@ public abstract class Input : Closeable {
         return limit
     }
 
-    private fun readThroughPreview(bytes: Bytes): Int {
+    private fun fillAndStoreInPreview(bytes: Bytes): Int {
         val nextIndex = previewIndex + 1
 
         if (bytes.isAfterLast(nextIndex)) {
+            prepareNewBuffer()
             val fetched = fillFromSource() // received data can be empty
-            bytes.append(buffer, limit) // buffer can be empty
+            bytes.append(buffer, fetched) // buffer can be empty
             previewIndex = nextIndex
             return fetched
         }
@@ -531,11 +537,9 @@ public abstract class Input : Closeable {
     }
 
     private fun fillFromSource(): Int {
-        val source = bufferPool.borrow()
-        val fetched = fill(source)
-        limit = fetched
+        val fetched = fill(buffer, 0, buffer.size)
         position = 0
-        buffer = source
+        limit = fetched
         return fetched
     }
 
