1cb6f5ae40a33cb634c708547077b575f6f5cbeb
Iaroslav Postovalov <postovalovya@gmail.com>
2020-10-11 14:58:36 +0700

    Use Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+allprojects {
+    repositories {
+        mavenLocal()
+        jcenter()
+        maven("https://dl.bintray.com/kotlin/kotlinx")
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
