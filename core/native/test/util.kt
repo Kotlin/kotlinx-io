54924d54dfdc603543fc467522fecc9f5b25e4e4
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-05-26 12:43:23 +0200

    Fixed path tests on macos-arm64

--- core/native/test/util.kt
+++ core/native/test/util.kt
@@ -6,16 +6,28 @@ package kotlinx.io
 
 import kotlinx.cinterop.*
 import platform.posix.*
+import kotlin.random.Random
+import kotlin.system.getTimeMillis
 
 actual fun createTempFile(): String {
     val template = "tmp-XXXXXX"
     val path = mktemp(template.cstr) ?: throw IOException("Filed to create temp file: ${strerror(errno)}")
+    // mktemp don't work on MacOS 13+ (as well as mkstemp), at least the way it's expected.
+    if (path.toKString() == "") {
+        val tmpDir = getenv("TMPDIR")?.toKString() ?: getenv("TMP")?.toKString() ?: ""
+        val rnd = Random(getTimeMillis())
+        var path: String
+        do {
+            path = "$tmpDir/tmp-${rnd.nextInt()}"
+        } while (access(path, F_OK) == 0)
+        return path
+    }
     return path.toKString()
 }
 
 actual fun deleteFile(path: String) {
     if (access(path, F_OK) != 0) throw IOException("File does not exist: $path")
     if (remove(path) != 0) {
-        throw IOException("Failed to delete file $path: ${strerror(errno)}")
+        throw IOException("Failed to delete file $path: ${strerror(errno)?.toKString()}")
     }
 }
