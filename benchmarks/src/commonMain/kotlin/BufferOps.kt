4a5b79bfde113aff8d30bf000a7747b1062ab88a
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-06-07 13:45:19 +0200

    Add benchmarks on java.io/nio integration and byte array reading

--- benchmarks/src/commonMain/kotlin/BufferOps.kt
+++ benchmarks/src/commonMain/kotlin/BufferOps.kt
@@ -341,3 +341,42 @@ open class BufferGetBenchmark {
     @Benchmark
     fun get() = buffer[offset]
 }
+
+open class BufferReadWriteByteArray: BufferRWBenchmarkBase() {
+    private var inputArray = ByteArray(0)
+    private var outputArray = ByteArray(0)
+
+    @Param("1", "1024", (SEGMENT_SIZE_IN_BYTES * 3).toString())
+    var size: Int = 0
+
+    @Setup
+    fun allocateArrays() {
+        inputArray = ByteArray(size)
+        outputArray = ByteArray(size)
+    }
+
+    @Benchmark
+    fun benchmark(blackhole: Blackhole) {
+        buffer.write(inputArray)
+        buffer.readFully(outputArray)
+        blackhole.consume(outputArray)
+    }
+}
+
+open class BufferReadNewByteArray: BufferRWBenchmarkBase() {
+    private var inputArray = ByteArray(0)
+
+    @Param("1", "1024", (SEGMENT_SIZE_IN_BYTES * 3).toString())
+    var size: Int = 0
+
+    @Setup
+    fun allocateArray() {
+        inputArray = ByteArray(size)
+    }
+
+    @Benchmark
+    fun benchmark(): ByteArray {
+        buffer.write(inputArray)
+        return buffer.readByteArray(size.toLong())
+    }
+}
