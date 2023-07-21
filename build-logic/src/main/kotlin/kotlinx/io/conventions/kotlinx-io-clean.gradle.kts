8324ee454cdfdd898cc6900d195722a9535cc03f
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-07-21 11:15:59 +0200

    Apply base plugin and reuse the clean task defined in it

--- build-logic/src/main/kotlin/kotlinx/io/conventions/kotlinx-io-clean.gradle.kts
+++ build-logic/src/main/kotlin/kotlinx/io/conventions/kotlinx-io-clean.gradle.kts
@@ -3,13 +3,15 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
-tasks {
+plugins {
+    base
+}
+
+tasks.clean {
     // Workaround for https://youtrack.jetbrains.com/issue/KT-58303:
     // the `clean` task can't delete the expanded.lock file on Windows as it's still held by Gradle, failing the build
-    named("clean", Delete::class) {
-        setDelete(layout.buildDirectory.asFileTree.matching {
-            exclude("tmp/.cache/expanded/expanded.lock")
-        })
-    }
+    setDelete(layout.buildDirectory.asFileTree.matching {
+        exclude("tmp/.cache/expanded/expanded.lock")
+    })
 }
 
