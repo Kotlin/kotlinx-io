6d034e9f462c4f0090f96d4ba263695f46208119
Jeff Lockhart <jeffdgr8@gmail.com>
2023-08-02 11:08:25 -0600

    Check for NSStreamStatusAtEnd after read

--- core/apple/src/SourcesApple.kt
+++ core/apple/src/SourcesApple.kt
@@ -72,7 +72,7 @@ private class SourceNSInputStream(
             }
             val toRead = minOf(maxLength.toLong(), source.buffer.size, Int.MAX_VALUE.toLong()).toInt()
             val read = source.buffer.readAtMostTo(buffer, toRead).convert<NSInteger>()
-            status = NSStreamStatusOpen
+            status = if (source.exhausted()) NSStreamStatusAtEnd else NSStreamStatusOpen
             checkBytes()
             return read
         } catch (e: Exception) {
