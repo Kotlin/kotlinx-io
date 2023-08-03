3bebf5884a258023957e48063fe41f4baed65f45
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-08-03 15:21:11 +0200

    Added comments regarding the indexOf algorithm

--- core/common/src/Buffers.kt
+++ core/common/src/Buffers.kt
@@ -56,17 +56,23 @@ public fun Buffer.indexOf(byte: Byte, startIndex: Long = 0, endIndex: Long = siz
         var segment = seg!!
         var offset = o
         do {
+            check(endOffset > offset)
             val idx = segment.indexOf(
                 byte,
+                // If start index within this segment, the diff will be positive and
+                // we'll scan the segment starting from the corresponding offset.
+                // Otherwise, the diff will be negative and we'll scan the segment from the beginning.
                 maxOf((startIndex - offset).toInt(), 0),
+                // If endOffset is within this segment - scan until it, otherwise - scan whole segment.
                 minOf(segment.size, (endOffset - offset).toInt())
             )
             if (idx != -1) {
+                // offset corresponds to the segment's start, idx - to offset within the segment.
                 return offset + idx.toLong()
             }
             offset += segment.size
             segment = segment.next!!
-        } while (segment !== head && offset < minOf(size, endOffset))
+        } while (segment !== head && offset < endOffset)
         return -1L
     }
 }
