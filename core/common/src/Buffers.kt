9445e5ab26fc9643e624f60e587af8563a576334
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-08-03 12:53:21 +0200

    Improve Source.indexOf performance

--- core/common/src/Buffers.kt
+++ core/common/src/Buffers.kt
@@ -27,3 +27,46 @@ public fun Buffer.snapshot(): ByteString {
         } while (curr !== head)
     }
 }
+
+/**
+ * Returns an index of [byte] first occurrence in the range of [startIndex] to [endIndex],
+ * or `-1` when the range doesn't contain [byte].
+ *
+ * The scan terminates at either [endIndex] or buffers' exhaustion, whichever comes first.
+ *
+ * @param byte the value to find.
+ * @param startIndex the start of the range (inclusive) to find [byte], `0` by default.
+ * @param endIndex the end of the range (exclusive) to find [byte], [Buffer.size] by default.
+ *
+ * @throws IllegalStateException when the source is closed.
+ * @throws IllegalArgumentException when `startIndex > endIndex` or either of indices is negative.
+ *
+ * @sample kotlinx.io.samples.KotlinxIoCoreCommonSamples.indexOfByteSample
+ */
+public fun Buffer.indexOf(byte: Byte, startIndex: Long = 0, endIndex: Long = size): Long {
+    // For compatibility with Source.indexOf accept endIndices greater than size and truncate them.
+    val endOffset = minOf(endIndex, size)
+    checkBounds(size, startIndex, endOffset)
+    if (startIndex == endOffset) return -1L
+
+    var segment = head!!
+    var offset = 0
+    do {
+        if (offset + segment.size <= startIndex) {
+            offset += segment.size
+            segment = segment.next!!
+        } else {
+            val idx = segment.indexOf(
+                byte,
+                maxOf((startIndex - offset).toInt(), 0),
+                minOf(segment.size, (endOffset - offset).toInt())
+            )
+            if (idx != -1) {
+                return offset + idx.toLong()
+            }
+            offset += segment.size
+            segment = segment.next!!
+        }
+    } while (segment !== head && offset < minOf(size, endOffset))
+    return -1L
+}
