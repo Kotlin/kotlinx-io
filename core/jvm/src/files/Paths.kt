2cb1d7dbe9a4a62fe8f2a97721fd8ee29591c594
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-08-10 13:02:53 +0200

    Use java.io.File instead of java.nio.files.Path
    Closes #202

--- core/jvm/src/files/Paths.kt
+++ core/jvm/src/files/Paths.kt
@@ -6,15 +6,14 @@
 package kotlinx.io.files
 
 import kotlinx.io.*
+import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.nio.file.*
-import java.nio.file.Path as NioPath
 
-public actual class Path internal constructor(internal val nioPath: NioPath)
+public actual class Path internal constructor(internal val file: File)
 
-public actual fun Path(path: String): Path = Path(Paths.get(path))
+public actual fun Path(path: String): Path = Path(File(path))
 
-public actual fun Path.source(): Source = FileInputStream(nioPath.toFile()).asSource().buffered()
+public actual fun Path.source(): Source = FileInputStream(file).asSource().buffered()
 
-public actual fun Path.sink(): Sink = FileOutputStream(nioPath.toFile()).asSink().buffered()
+public actual fun Path.sink(): Sink = FileOutputStream(file).asSink().buffered()
