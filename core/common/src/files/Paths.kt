b75f14cd7c75fb86a943f18081967e6a78b8c45b
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-09-13 12:01:10 +0200

    Fixed version in kdoc

--- core/common/src/files/Paths.kt
+++ core/common/src/files/Paths.kt
@@ -99,7 +99,7 @@ public fun Path(base: Path, vararg parts: String): Path {
 /**
  * Returns [RawSource] for the given file or throws if path is not a file or does not exist
  *
- * Use of this method is deprecated with warning since kotlinx-io 0.2.3. The method will be removed in 0.3.0.
+ * Use of this method is deprecated with warning since kotlinx-io 0.2.2. The method will be removed in 0.3.0.
  */
 @Deprecated(
     message = "Use FileSystem.source instead",
@@ -116,7 +116,7 @@ public fun Path.source(): Source = SystemFileSystem.source(this).buffered()
  * Returns [RawSink] for the given path, creates file if it doesn't exist, throws if it's a directory,
  * overwrites contents.
  *
- * Use of this method is deprecated with warning since kotlinx-io 0.2.3. The method will be removed in 0.3.0.
+ * Use of this method is deprecated with warning since kotlinx-io 0.2.2. The method will be removed in 0.3.0.
  */
 @Deprecated(
     message = "Use FileSystem.sink instead",
