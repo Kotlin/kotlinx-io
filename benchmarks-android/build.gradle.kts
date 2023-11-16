da2a36aa7f2977eb3e948911a54125f66eba0a41
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-11-16 12:48:59 +0100

    Added Android benchmarks module and ported a few benchmarks

---
+++ benchmarks-android/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.androidx.benchmark)
+    alias(libs.plugins.kotlin.android)
+}
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+android {
+    namespace = "kotlinx.io.benchmark.android"
+    compileSdk = 33
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.benchmark.junit4.AndroidBenchmarkRunner"
+        testInstrumentationRunnerArguments["androidx.benchmark.output.enable"] = "true"
+    }
+
+    testBuildType = "release"
+    buildTypes {
+        debug {
+            // Since isDebuggable can"t be modified by gradle for library modules,
+            // it must be done in a manifest - see src/androidTest/AndroidManifest.xml
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "benchmark-proguard-rules.pro")
+        }
+        release {
+            isDefault = true
+        }
+    }
+}
+
+dependencies {
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.benchmark.junit4)
+    androidTestImplementation(project(":kotlinx-io-core"))
+}
