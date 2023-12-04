f35facbd2f725fbacc2215cd8b81001a6a69651b
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2023-12-04 12:57:12 +0100

    disable kover coverage limits

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,10 +62,10 @@ kover {
         verify {
             rule {
                 // TODO: rollback to 95
-            minBound(94, CoverageUnit.LINE)
+            // minBound(94, CoverageUnit.LINE)
 
                 // we allow lower branch coverage, because not all checks in the internal code lead to errors
-                minBound(80, CoverageUnit.BRANCH)
+                // minBound(80, CoverageUnit.BRANCH)
             }
         }
     }
