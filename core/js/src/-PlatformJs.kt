0411831cb4ec7c0551b93b740a19e08c3212906f
Zongle Wang <wangzongler@gmail.com>
2024-03-12 19:01:07 +0800

    Remove exception primary constructors (#281)

--- core/js/src/-PlatformJs.kt
+++ core/js/src/-PlatformJs.kt
@@ -9,19 +9,20 @@ import kotlinx.io.internal.commonAsUtf8ToByteArray
 
 internal actual fun String.asUtf8ToByteArray(): ByteArray = commonAsUtf8ToByteArray()
 
-public actual open class IOException actual constructor(
-    message: String?,
-    cause: Throwable?
-) : Exception(message, cause) {
-    public actual constructor(message: String?) : this(message, null)
+public actual open class IOException : Exception {
+    public actual constructor() : super()
 
-    public actual constructor(cause: Throwable?) : this(null, cause)
+    public actual constructor(message: String?) : super(message)
 
-    public actual constructor() : this(null, null)
+    public actual constructor(cause: Throwable?) : super(cause)
+
+    public actual constructor(message: String?, cause: Throwable?) : super(message, cause)
 }
 
-public actual open class EOFException actual constructor(message: String?) : IOException(message) {
-    public actual constructor() : this(null)
+public actual open class EOFException : IOException {
+    public actual constructor() : super()
+
+    public actual constructor(message: String?) : super(message)
 }
 
 @Suppress("ACTUAL_WITHOUT_EXPECT")
