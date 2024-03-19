e918c0de3bb89e83954d6ffc3e661bc8032c52f2
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-03-19 11:21:13 +0100

    Fixed a test

--- core/common/test/files/SmokeFileTest.kt
+++ core/common/test/files/SmokeFileTest.kt
@@ -447,9 +447,13 @@ class SmokeFileTest {
     fun pathNormalize() {
         assertEquals(Path(""), Path("").normalized())
         assertEquals(Path("${SystemPathSeparator}a"), Path("/////////////a/").normalized())
-        assertEquals(Path("${SystemPathSeparator}e"), Path("/a/b/../c/../d/../../../e").normalized())
         assertEquals(Path("..", "..", "e"), Path("a/b/../c/../d/../../../../e").normalized())
         assertEquals(Path("a"), Path("a/././././").normalized())
+
+        if (!isWindows) {
+            // On Windows, this path is usually considered relative
+            assertEquals(Path("${SystemPathSeparator}e"), Path("/a/b/../c/../d/../../../e").normalized())
+        }
     }
 
     private fun constructAbsolutePath(vararg parts: String): String {
