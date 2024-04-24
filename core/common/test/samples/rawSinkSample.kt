a904c8ad0ad83939277f85ab28ad54fbc33b0682
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-04-24 10:06:08 +0200

    Don't refer to classes in @sample tags
    Starting from 1.9.20, Dokka no longer supports
    classes being referred in @sample tag.

--- core/common/test/samples/rawSinkSample.kt
+++ core/common/test/samples/rawSinkSample.kt
@@ -27,44 +27,43 @@ private fun generateCrc32Table(): UIntArray {
     return table
 }
 
-/**
- * Sink calculating CRC-32 code for all the data written to it and sending this data to the upstream afterward.
- * The CRC-32 value could be obtained using [crc32] method.
- *
- * See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for more information about CRC-32.
- */
-@OptIn(ExperimentalUnsignedTypes::class)
-class CRC32Sink(private val upstream: RawSink): RawSink {
-    private val tempBuffer = Buffer()
-    private val crc32Table = generateCrc32Table()
-    private var crc32: UInt = 0xffffffffU
-
-    private fun update(value: Byte) {
-        val index = value.xor(crc32.toByte()).toUByte()
-        crc32 = crc32Table[index.toInt()].xor(crc32.shr(8))
-    }
-
-    fun crc32(): UInt = crc32.xor(0xffffffffU)
-
-    override fun write(source: Buffer, byteCount: Long) {
-        source.copyTo(tempBuffer, 0, byteCount)
-
-        while (!tempBuffer.exhausted()) {
-            update(tempBuffer.readByte())
-        }
-
-        upstream.write(source, byteCount)
-    }
-
-    override fun flush() = upstream.flush()
-
-    override fun close() = upstream.close()
-}
-
 class Crc32Sample {
-    @OptIn(ExperimentalStdlibApi::class)
+    @OptIn(ExperimentalStdlibApi::class, ExperimentalUnsignedTypes::class)
     @Test
     fun crc32() {
+        /**
+         * Sink calculating CRC-32 code for all the data written to it and sending this data to the upstream afterward.
+         * The CRC-32 value could be obtained using [crc32] method.
+         *
+         * See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for more information about CRC-32.
+         */
+        class CRC32Sink(private val upstream: RawSink): RawSink {
+            private val tempBuffer = Buffer()
+            private val crc32Table = generateCrc32Table()
+            private var crc32: UInt = 0xffffffffU
+
+            private fun update(value: Byte) {
+                val index = value.xor(crc32.toByte()).toUByte()
+                crc32 = crc32Table[index.toInt()].xor(crc32.shr(8))
+            }
+
+            fun crc32(): UInt = crc32.xor(0xffffffffU)
+
+            override fun write(source: Buffer, byteCount: Long) {
+                source.copyTo(tempBuffer, 0, byteCount)
+
+                while (!tempBuffer.exhausted()) {
+                    update(tempBuffer.readByte())
+                }
+
+                upstream.write(source, byteCount)
+            }
+
+            override fun flush() = upstream.flush()
+
+            override fun close() = upstream.close()
+        }
+
         val crc32Sink = CRC32Sink(discardingSink())
 
         crc32Sink.buffered().use {
