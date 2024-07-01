825dd8e7587b389641a1da3d5b3a4b74d465b4a2
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-07-01 12:17:56 +0200

    Fixed how buffer's head is updated after the split

--- core/common/src/Buffer.kt
+++ core/common/src/Buffer.kt
@@ -501,11 +501,7 @@ public class Buffer : Source, Sink {
                 } else {
                     // We're going to need another segment. Split the source's head
                     // segment in two, then move the first of those two to this buffer.
-                    val newHead = source.head!!.split(remainingByteCount.toInt())
-                    if (source.head == source.tail) {
-                        source.tail = newHead
-                    }
-                    source.head = newHead
+                    source.head = source.head!!.split(remainingByteCount.toInt())
                 }
             }
 
