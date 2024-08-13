ecb1d38afa6f0026766d00b54115b8dc638b8cdf
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-08-13 08:12:52 -0400

    Updated Gradle and some of dependencies (#353)
    * Updated Gradle and some of dependencies
    
    Co-authored-by: Zongle Wang <wangzongler@gmail.com>

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -4,7 +4,7 @@
  */
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.7.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
 
 dependencyResolutionManagement {
