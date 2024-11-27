1a7c186bb8bb771d5931aec30805dbbab461a0bd
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-11-27 18:04:35 -0500

    Optimized implementations

--- integration/kotlinx-io-okio/common/src/OkioAdapters.kt
+++ integration/kotlinx-io-okio/common/src/OkioAdapters.kt
@@ -6,8 +6,11 @@
 package kotlinx.io.okio
 
 import kotlinx.io.Buffer
+import kotlinx.io.InternalIoApi
 import kotlinx.io.RawSink
 import kotlinx.io.RawSource
+import kotlinx.io.Sink
+import kotlinx.io.Source
 import kotlinx.io.UnsafeIoApi
 import kotlinx.io.bytestring.ByteString
 import kotlinx.io.bytestring.isEmpty
@@ -15,6 +18,7 @@ import kotlinx.io.bytestring.unsafe.UnsafeByteStringApi
 import kotlinx.io.bytestring.unsafe.UnsafeByteStringOperations
 import kotlinx.io.unsafe.UnsafeBufferOperations
 import okio.ByteString.Companion.toByteString
+import okio.Timeout
 import kotlin.math.min
 
 private inline fun <T> withOkio2KxIOExceptionMapping(block: () -> T): T {
@@ -37,54 +41,41 @@ private inline fun <T> withKxIO2OkioExceptionMapping(block: () -> T): T {
     }
 }
 
-/**
- * Returns a [okio.Source] backed by this [kotlinx.io.RawSource].
- *
- * Closing one of these sources will also close another one.
- */
-public fun RawSource.asOkioSource(): okio.Source = object : okio.Source {
-    private val buffer = Buffer()
-
-    override fun close() = withKxIO2OkioExceptionMapping {
-        this@asOkioSource.close()
-    }
+private abstract class RawSource2OkioSourceAdapterBase<T : RawSource>(
+    val source: T,
+    val buffer: Buffer
+) : okio.Source {
+    override fun close() = withKxIO2OkioExceptionMapping(source::close)
+    override fun timeout(): Timeout = Timeout.NONE
 
     override fun read(sink: okio.Buffer, byteCount: Long): Long = withKxIO2OkioExceptionMapping {
-        val read = this@asOkioSource.readAtMostTo(buffer, byteCount)
-        if (read <= 0) return read
+        val fetched = fetchData(byteCount)
+        if (fetched <= 0) return fetched
 
-        while (!buffer.exhausted()) {
+        var remaining = fetched
+
+        while (remaining > 0) {
             @OptIn(UnsafeIoApi::class)
             UnsafeBufferOperations.readFromHead(buffer) { data, from, to ->
-                val len = to - from
+                val len = min((to - from).toLong(), remaining).toInt()
                 sink.write(data, from, len)
+                remaining -= len
                 len
             }
         }
-        return read
+        return fetched
     }
 
-    override fun timeout(): okio.Timeout = okio.Timeout.NONE
+    abstract fun fetchData(fetchAtMost: Long): Long
 }
 
-/**
- * Returns a [okio.Sink] backed by this [kotlinx.io.RawSink].
- *
- * Closing one of these sinks will also close another one.
- */
-public fun RawSink.asOkioSink(): okio.Sink = object : okio.Sink {
-    private val buffer = Buffer()
-
-    override fun close() = withKxIO2OkioExceptionMapping {
-        this@asOkioSink.close()
-    }
-
-    override fun flush() = withKxIO2OkioExceptionMapping {
-        this@asOkioSink.flush()
-
-    }
-
-    override fun timeout(): okio.Timeout = okio.Timeout.NONE
+private abstract class RawSink2OkioSinkAdapterBase<T : RawSink>(
+    val sink: T,
+    val buffer: Buffer
+) : okio.Sink {
+    override fun close() = withKxIO2OkioExceptionMapping(sink::close)
+    override fun flush() = withKxIO2OkioExceptionMapping(sink::flush)
+    override fun timeout(): Timeout = Timeout.NONE
 
     override fun write(source: okio.Buffer, byteCount: Long) = withKxIO2OkioExceptionMapping {
         require(source.size >= byteCount) {
@@ -102,55 +93,51 @@ public fun RawSink.asOkioSink(): okio.Sink = object : okio.Sink {
                 read
             }
         }
-        this@asOkioSink.write(buffer, buffer.size)
+        flushBuffer()
+    }
+
+    abstract fun flushBuffer()
+}
+
+private class RawSource2OkioSourceAdapter(source: RawSource) :
+    RawSource2OkioSourceAdapterBase<RawSource>(source, Buffer()) {
+    override fun fetchData(fetchAtMost: Long): Long = source.readAtMostTo(buffer, fetchAtMost)
+}
+
+@OptIn(InternalIoApi::class)
+private class Source2OkioSourceAdapter(source: Source) :
+    RawSource2OkioSourceAdapterBase<Source>(source, source.buffer) {
+    override fun fetchData(fetchAtMost: Long): Long {
+        // if there's no cached data and source is exhausted, we're done
+        if (buffer.exhausted() && source.exhausted()) {
+            return -1L
+        }
+        // the buffer is empty, but the source is not exhausted yet
+        if (buffer.size == 0L) {
+            return 0L
+        }
+
+        return min(fetchAtMost, buffer.size)
     }
 }
 
-/**
- * Returns a [kotlinx.io.RawSink] backed by this [okio.Sink].
- *
- * Closing one of these sinks will also close another one.
- */
-public fun okio.Sink.asKotlinxIoRawSink(): RawSink = object : RawSink {
-    private val buffer = okio.Buffer()
-
-    override fun write(source: Buffer, byteCount: Long) = withOkio2KxIOExceptionMapping {
-        require(source.size >= byteCount) {
-            "Buffer does not contain enough bytes to write. Requested $byteCount, actual size is ${source.size}"
-        }
-        var remaining = byteCount
-        while (remaining > 0) {
-            @OptIn(UnsafeIoApi::class)
-            UnsafeBufferOperations.readFromHead(source) { data, from, to ->
-                val toRead = min((to - from).toLong(), remaining).toInt()
-                remaining -= toRead
-                buffer.write(data, from, toRead)
-                toRead
-            }
-            this@asKotlinxIoRawSink.write(buffer, byteCount)
-        }
-    }
-
-    override fun flush() = withOkio2KxIOExceptionMapping {
-        this@asKotlinxIoRawSink.flush()
-    }
-
-    override fun close() = withOkio2KxIOExceptionMapping {
-        this@asKotlinxIoRawSink.close()
-    }
+private class RawSink2OkioSinkAdapter(sink: RawSink) : RawSink2OkioSinkAdapterBase<RawSink>(sink, Buffer()) {
+    override fun flushBuffer() = sink.write(buffer, buffer.size)
 }
 
-/**
- * Returns a [kotlinx.io.RawSource] backed by this [okio.Source].
- *
- * Closing one of these sources will also close another one.
- */
-public fun okio.Source.asKotlinxIoRawSource(): RawSource = object : RawSource {
-    private val buffer = okio.Buffer()
+@OptIn(InternalIoApi::class)
+private class Sink2OkioSinkAdapter(sink: Sink) : RawSink2OkioSinkAdapterBase<Sink>(sink, sink.buffer) {
+    override fun flushBuffer() = sink.hintEmit()
+}
 
-    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long = withOkio2KxIOExceptionMapping {
-        val readBytes = this@asKotlinxIoRawSource.read(buffer, byteCount)
+private abstract class OkioSource2RawSourceAdapterBase<T : okio.Source>(
+    val source: T,
+    val buffer: okio.Buffer
+) : RawSource {
+    override fun close() = withOkio2KxIOExceptionMapping(source::close)
 
+    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
+        val readBytes = fetchData(byteCount)
         if (readBytes <= 0L) return readBytes
 
         var remaining = readBytes
@@ -168,9 +155,104 @@ public fun okio.Source.asKotlinxIoRawSource(): RawSource = object : RawSource {
         return readBytes
     }
 
-    override fun close() = withOkio2KxIOExceptionMapping {
-        this@asKotlinxIoRawSource.close()
+    abstract fun fetchData(byteCount: Long): Long
+}
+
+private abstract class OkioSink2RawSinkAdapterBase<T : okio.Sink>(
+    val sink: T,
+    val buffer: okio.Buffer
+) : RawSink {
+    override fun flush() = withOkio2KxIOExceptionMapping(sink::flush)
+    override fun close() = withOkio2KxIOExceptionMapping(sink::close)
+
+    override fun write(source: Buffer, byteCount: Long) {
+        require(source.size >= byteCount) {
+            "Buffer does not contain enough bytes to write. Requested $byteCount, actual size is ${source.size}"
+        }
+        var remaining = byteCount
+        while (remaining > 0) {
+            @OptIn(UnsafeIoApi::class)
+            UnsafeBufferOperations.readFromHead(source) { data, from, to ->
+                val toRead = min((to - from).toLong(), remaining).toInt()
+                remaining -= toRead
+                buffer.write(data, from, toRead)
+                toRead
+            }
+        }
+
+        flushBuffer()
     }
+
+    abstract fun flushBuffer()
+}
+
+private class OkioSink2RawSinkAdapter(sink: okio.Sink) : OkioSink2RawSinkAdapterBase<okio.Sink>(sink, okio.Buffer()) {
+    override fun flushBuffer() = sink.write(buffer, buffer.size)
+}
+
+private class OkioBufferedSink2RawSinkAdapter(sink: okio.BufferedSink) :
+    OkioSink2RawSinkAdapterBase<okio.BufferedSink>(sink, sink.buffer) {
+    override fun flushBuffer() {
+        sink.emitCompleteSegments()
+    }
+}
+
+private class OkioSource2RawSourceAdapter(source: okio.Source) :
+    OkioSource2RawSourceAdapterBase<okio.Source>(source, okio.Buffer()) {
+    override fun fetchData(byteCount: Long): Long = source.read(buffer, byteCount)
+}
+
+private class OkioBufferedSource2RawSourceAdapter(source: okio.BufferedSource) :
+    OkioSource2RawSourceAdapterBase<okio.BufferedSource>(source, source.buffer) {
+    override fun fetchData(byteCount: Long): Long {
+        if (buffer.exhausted() && source.exhausted()) {
+            return -1L
+        }
+        if (buffer.size == 0L) {
+            return 0L
+        }
+        return min(buffer.size, byteCount)
+    }
+}
+
+/**
+ * Returns a [okio.Source] backed by this [kotlinx.io.RawSource].
+ *
+ * Closing one of these sources will also close another one.
+ */
+public fun RawSource.asOkioSource(): okio.Source = when (this) {
+    is Source -> Source2OkioSourceAdapter(this)
+    else -> RawSource2OkioSourceAdapter(this)
+}
+
+/**
+ * Returns a [okio.Sink] backed by this [kotlinx.io.RawSink].
+ *
+ * Closing one of these sinks will also close another one.
+ */
+public fun RawSink.asOkioSink(): okio.Sink = when (this) {
+    is Sink -> Sink2OkioSinkAdapter(this)
+    else -> RawSink2OkioSinkAdapter(this)
+}
+
+/**
+ * Returns a [kotlinx.io.RawSink] backed by this [okio.Sink].
+ *
+ * Closing one of these sinks will also close another one.
+ */
+public fun okio.Sink.asKotlinxIoRawSink(): RawSink = when (this) {
+    is okio.BufferedSink -> OkioBufferedSink2RawSinkAdapter(this)
+    else -> OkioSink2RawSinkAdapter(this)
+}
+
+/**
+ * Returns a [kotlinx.io.RawSource] backed by this [okio.Source].
+ *
+ * Closing one of these sources will also close another one.
+ */
+public fun okio.Source.asKotlinxIoRawSource(): RawSource = when (this) {
+    is okio.BufferedSource -> OkioBufferedSource2RawSourceAdapter(this)
+    else -> OkioSource2RawSourceAdapter(this)
 }
 
 /**
