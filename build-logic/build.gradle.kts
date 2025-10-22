dc2b7c56938e18041dd16503058dd231278a9669
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-10-22 12:40:47 -0400

    Apply power-assert plugin (#478)

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 JetBrains s.r.o. and respective authors and developers.
+ * Copyright 2017-2025 JetBrains s.r.o. and respective authors and developers.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENCE file.
  */
 
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.power.assert.plugin)
     implementation(libs.dokka.gradle.plugin)
     implementation(libs.animalsniffer.gradle.plugin)
 }
