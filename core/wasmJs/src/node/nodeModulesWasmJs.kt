2e2d7f3a303a5ea75eefe7152be9a4a22c4b863a
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2026-05-07 18:26:51 +0200

    Use node builtin modules with @JsModule, not via magic script

--- core/wasmJs/src/node/nodeModulesWasmJs.kt
+++ core/wasmJs/src/node/nodeModulesWasmJs.kt
@@ -7,55 +7,14 @@
 
 package kotlinx.io.node
 
-@JsFun("""
-    (globalThis.module = (typeof process !== 'undefined') && (process.release.name === 'node') ?
-        await import(/* webpackIgnore: true */'node:module') : void 0, () => {})
-""")
-internal external fun persistModule()
+@JsModule("node:buffer")
+internal actual external val buffer: BufferModule
 
-@JsFun("""() => { 
-    const importMeta = import.meta;
-    return globalThis.module.default.createRequire(importMeta.url);
-}
-""")
-internal external fun getRequire(): JsAny
+@JsModule("node:os")
+internal actual external val os: Os
 
-private val require = persistModule().let { getRequire() }
+@JsModule("node:path")
+internal actual external val path: Path
 
-@JsFun("""
-    (require, mod) => {
-         try {
-             let m = require(mod);
-             if (m) return m;
-             return null;
-         } catch (e) {
-             return null;
-         }
-    }
-""")
-internal external fun requireModule(require: JsAny, mod: String): JsAny?
-
-internal fun loadModule(name: String): JsAny {
-    val mod = requireModule(require, name) ?: throw UnsupportedOperationException("Module '$name' could not be imported")
-    return mod
-}
-
-internal actual val buffer: BufferModule by lazy {
-    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-    loadModule("buffer") as BufferModule
-}
-
-internal actual val os: Os  by lazy {
-    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-    loadModule("os") as Os
-}
-
-internal actual val path: Path by lazy {
-    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-    loadModule("path") as Path
-}
-
-internal actual val fs: Fs by lazy {
-    @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
-    loadModule("fs") as Fs
-}
+@JsModule("node:fs")
+internal actual external val fs: Fs
