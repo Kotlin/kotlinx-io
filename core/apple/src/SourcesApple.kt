75d61c2259976b8e55a4934c5954bc4f64fe0666
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-08-20 16:43:06 +0300

    ~~ Use the new commonization algorithm
    Drop the now unnecessary changes.

--- core/apple/src/SourcesApple.kt
+++ core/apple/src/SourcesApple.kt
@@ -75,12 +75,12 @@ private class SourceNSInputStream(
     }
 
     override fun read(buffer: CPointer<uint8_tVar>?, maxLength: NSUInteger): NSInteger {
-        if (streamStatus != NSStreamStatusOpen && streamStatus != NSStreamStatusAtEnd || buffer == null) return (-1).convert()
+        if (streamStatus != NSStreamStatusOpen && streamStatus != NSStreamStatusAtEnd || buffer == null) return -1
         status = NSStreamStatusReading
         try {
             if (source.exhausted()) {
                 status = NSStreamStatusAtEnd
-                return 0.convert()
+                return 0
             }
             val toRead = minOf(maxLength.toLong(), source.buffer.size, Int.MAX_VALUE.toLong()).toInt()
             val read = source.buffer.readAtMostTo(buffer, toRead).convert<NSInteger>()
@@ -90,7 +90,7 @@ private class SourceNSInputStream(
         } catch (e: Exception) {
             error = e.toNSError()
             postEvent(NSStreamEventErrorOccurred)
-            return (-1).convert()
+            return -1
         }
     }
 
--- core/native/src/files/PathsNative.kt
+++ core/native/src/files/PathsNative.kt
@@ -10,7 +10,6 @@ package kotlinx.io.files
 import kotlinx.cinterop.*
 import kotlinx.io.*
 import platform.posix.*
-import kotlin.let
 
 /*
  * The very base skeleton just to play around
@@ -111,16 +110,14 @@ internal fun variantFread(
     target: CPointer<ByteVarOf<Byte>>,
     byteCount: UInt,
     file: CPointer<FILE>
-): UInt = fread(target, 1u, byteCount.convert(), file)
-    .let { @Suppress("REDUNDANT_CALL_OF_CONVERSION_METHOD") it.toUInt() }
+): UInt = fread(target, 1u, byteCount.convert(), file).convert()
 
 @OptIn(UnsafeNumber::class)
 internal fun variantFwrite(
     source: CPointer<ByteVar>,
     byteCount: UInt,
     file: CPointer<FILE>
-): UInt = fwrite(source, 1u, byteCount.convert(), file)
-    .let { @Suppress("REDUNDANT_CALL_OF_CONVERSION_METHOD") it.toUInt() }
+): UInt = fwrite(source, 1u, byteCount.convert(), file).convert()
 
 internal class FileSink(
     private val file: CPointer<FILE>
